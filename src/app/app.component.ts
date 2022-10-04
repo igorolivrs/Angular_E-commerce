098a7f9186cd592d38db3bfae9829a2925a9a251
Igor Oliveira <igorsouza.psn@gmail.com>
2022-10-04 19:57:21 +0100

    Carousel and Card Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular_marketplace';
+
+  images = [
+    { imageSrc: '../assets/carousel-images/ryzen7000.jpg', imageAlt: 'AMD Ryzen series 7000' },
+    { imageSrc: '../assets/carousel-images/tp-link_tapo_d.jpg', imageAlt: 'Tp-Link Tapo' },
+    { imageSrc: '../assets/carousel-images/formuler_tv_d.jpg', imageAlt: 'IPTV Formuler' },
+    { imageSrc: '../assets/carousel-images/artics.jpg', imageAlt: 'SteelSeries Artics' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from './components/template/carousel/carousel.module';
 
 // Components
 import { HeaderComponent } from './components/template/header/header.component'
 import { FooterComponent } from './components/template/footer/footer.component';
+import { CardComponent } from './components/template/card/card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
